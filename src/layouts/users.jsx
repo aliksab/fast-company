eb8fd2012ab35cd07a63773873ce9fa121ba2220
aliksab <alexeysavich1112@gmail.com>
2023-08-10 15:34:23 +0900

    Авторизация и аутентификация часть 2

--- src/layouts/users.jsx
+++ src/layouts/users.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Redirect } from "react-router-dom";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/userListPage";
 import EditUserPage from "../components/page/editUserPage/editUserPage";
 import UserProvider from "../hooks/useUsers";
+import { useAuth } from "../hooks/useAuth";
 
 const UsersPage = () => {
   const { userId, edit } = useParams();
-
+  const { currentUser } = useAuth();
   return (
     <>
     <UserProvider>
-      {userId ? edit ? <EditUserPage /> : <UserPage id={userId}/> : <UsersListPage />}
+      {userId ? edit ? (userId === currentUser._id ? <EditUserPage /> : <Redirect to={`/users/${currentUser._id}/edit`} />) : <UserPage id={userId}/> : <UsersListPage />}
     </UserProvider>
     </>
   );
